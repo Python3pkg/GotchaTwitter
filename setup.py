efc789fb52cd59f0d1eec278c5dfbb44b8c7b222
PhantomKidding <phantomkidding@gmail.com>
2016-07-22 15:21:42 -0500

    0.07

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'gotchatwitter',
-    version = '0.0.6',
+    version = '0.0.7',
     keywords = ('twitter', 'python', 'crawler', 'web-scraping'),
     description = 'Crawling twitter in Python',
     license = 'MIT License',
