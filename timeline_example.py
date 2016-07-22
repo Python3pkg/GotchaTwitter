4efa58fb2a7b322c981bc858f6c9563e330d3f35
PhantomKidding <phantomkidding@gmail.com>
2016-07-22 15:15:33 -0500

    0.0.6

--- timeline_example.py
+++ timeline_example.py
@@ -1,4 +1,4 @@
-from pycrawlers.twitter import TwitterCrawler
+from gotchatwitter import GotchaTwitter
 import sys
 from glob import glob
 
@@ -18,7 +18,6 @@ if __name__ == '__main__':
 		# a = TwitterCrawler('timeline').set_input(file, start_value=start).set_output(file + '.csv').set_progress_bar(2).set_notifier('gmail').set_date_range('2016-02-06', '2016-02-10')
 		#a = TwitterCrawler('info').set_input(file).set_output(file + '.csv').set_notifier('gmail')
 		try:
-			a.crawl()
+			gt.crawl()
 		except KeyboardInterrupt:
-			a.record()
 			break
